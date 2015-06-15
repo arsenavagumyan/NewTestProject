9760953038568432bc939906014dc6d8c14a4ac8
unknown <arsen.avagumyan@Sis3w034.arm.synisys.com>
2015-06-15 10:16:08 +0400

    commit Arsen2

---
+++ src/main/java/Project.java
@@ -0,0 +1,23 @@
+/**
+ * Created by arsen.avagumyan on 6/15/2015.
+ */
+public class Project {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
